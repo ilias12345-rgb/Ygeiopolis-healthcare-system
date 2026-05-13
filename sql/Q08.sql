84e9fc366ba1bb13d81cbada2fa37a0e3ef3a286
katerinagerogiannopoulou <katarinageroyan@gmail.com>
2026-05-13 23:21:29 +0300

    Update Q08.sql

---
+++ sql/Q08.sql
@@ -0,0 +1,9 @@
+SELECT first_name, last_name, amka, personnel_type
+FROM personnel
+WHERE amka NOT IN (
+    SELECT personnel_amka
+    FROM shift_assignment sa
+    JOIN department_shift ds ON ds.shift_id = sa.shift_id
+    WHERE ds.shift_date = 2026-03-10
+        AND ds.department_id = 5
+    );
