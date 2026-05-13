cf9cdb352dbb85aa41445cef145293e3334a5357
katerinagerogiannopoulou <katarinageroyan@gmail.com>
2026-05-13 23:21:50 +0300

    Update Q09.sql

---
+++ sql/Q09.sql
@@ -0,0 +1,14 @@
+WITH patient_totals AS (
+    SELECT patient_amka, first_name, last_name, SUM(DATEDIFF(discharge_ts, admission_ts)) AS total_days
+    FROM patient_history
+    GROUP BY patient_amka, first_name, last_name
+)
+SELECT *
+FROM patient_totals
+WHERE total_days > 15
+AND total_days IN (
+    SELECT total_days
+    FROM patient_totals
+    GROUP BY total_days
+    HAVING COUNT(*) > 1
+);
