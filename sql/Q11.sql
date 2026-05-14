c464f064311af0c33eecec596d04768328e798ae
katerinagerogiannopoulou <katarinageroyan@gmail.com>
2026-05-14 23:40:06 +0300

    Update Q11.sql

--- sql/Q11.sql
+++ sql/Q11.sql
@@ -2,7 +2,7 @@ SELECT p.first_name, p.last_name, d.amka, COUNT(*) AS total_procedures
 FROM doctor d
 JOIN personnel p ON p.amka = d.amka
 JOIN procedure_event pe ON d.amka = pe.chief_surgeon_amka
-WHERE YEAR(pe.start_ts) = 2025
+WHERE YEAR(pe.start_ts) = YEAR(CURDATE())
 GROUP BY d.amka, p.first_name, p.last_name
 HAVING total_procedures + 5 <= (
     SELECT COUNT(*)
