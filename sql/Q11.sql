05ff0365ad6f4db367dc1f0d7fe5d7b670162907
ilias <iliasevangelou710@gmail.com>
2026-05-14 17:51:48 +0300

    Fix merged query compatibility

--- sql/Q11.sql
+++ sql/Q11.sql
@@ -1,8 +1,9 @@
-SELECT d.first_name, d.last_name, d.amka, COUNT(*) AS total_procedures
+SELECT p.first_name, p.last_name, d.amka, COUNT(*) AS total_procedures
 FROM doctor d
+JOIN personnel p ON p.amka = d.amka
 JOIN procedure_event pe ON d.amka = pe.chief_surgeon_amka
 WHERE YEAR(pe.start_ts) = 2025
-GROUP BY d.amka, d.first_name, d.last_name
+GROUP BY d.amka, p.first_name, p.last_name
 HAVING total_procedures + 5 <= (
     SELECT COUNT(*)
     FROM procedure_event
--- sql/Q15.sql
+++ sql/Q15.sql
@@ -1,11 +1,7 @@
 SELECT emergency_level, AVG(TIMESTAMPDIFF(MINUTE, arrival_ts, service_start_ts)) AS average_waiting_time, 
     COUNT(*) AS cases,  100.0 * SUM(CASE WHEN disposition = 'HOSPITALIZED' THEN 1 ELSE 0 END) / COUNT(*) AS hospitalization_percent,
-    CASE
-        WHEN referred_department_id IS NULL
-        THEN 'No referred department'
-        ELSE d.department_name
-    END AS department_name
+    COALESCE(d.department_name, 'No referred department') AS department_name
 FROM emergency_visit ev
 LEFT JOIN department d ON d.department_id = ev.referred_department_id
-GROUP BY emergency_level, d.department_name
+GROUP BY emergency_level, COALESCE(d.department_name, 'No referred department')
 ORDER BY emergency_level ASC;
