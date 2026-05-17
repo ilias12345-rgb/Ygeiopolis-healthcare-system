c74c75debac633f0d14d78ddf7a4b0d033022021
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2026-05-17 20:19:34 +0300

    Update Q09.sql

--- sql/Q09.sql
+++ sql/Q09.sql
@@ -1,15 +1,20 @@
 WITH patient_totals AS (
-    SELECT patient_amka, first_name, last_name, SUM(DATEDIFF(discharge_ts, admission_ts)) AS total_days
+    SELECT 
+        patient_amka, 
+        first_name, 
+        last_name, 
+        YEAR(admission_ts) AS hosp_year, /*Εξαγωγή του έτους*/
+        SUM(DATEDIFF(discharge_ts, admission_ts)) AS total_days
     FROM patient_history
-    GROUP BY patient_amka, first_name, last_name
+    GROUP BY patient_amka, first_name, last_name, YEAR(admission_ts) /* Ομαδοποίηση και ανά έτος */
 )
 SELECT *
 FROM patient_totals
 WHERE total_days > 15
-AND total_days IN (
-    SELECT total_days
+AND (hosp_year, total_days) IN ( /* Έλεγχος σύμπτωσης ημερών ΜΕΣΑ στο ίδιο έτος */
+    SELECT hosp_year, total_days
     FROM patient_totals
-    GROUP BY total_days
+    GROUP BY hosp_year, total_days
     HAVING COUNT(*) > 1
 )
-ORDER BY total_days;
+ORDER BY hosp_year, total_days;
