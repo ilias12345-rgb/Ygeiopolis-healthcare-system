31758449ae0eae438b1c2bff369dc1243161f6fe
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2026-05-18 13:08:53 +0300

    Update Q15.sql

--- sql/Q15.sql
+++ sql/Q15.sql
@@ -2,7 +2,7 @@ SELECT
     ev.emergency_level, 
     ROUND(AVG(TIMESTAMPDIFF(MINUTE, ev.arrival_ts, ev.service_start_ts)), 2) AS average_waiting_time, 
     COUNT(*) AS cases_per_department,
-    ROUND(100.0 * SUM(CASE WHEN ev.disposition = 'HOSPITALIZED' THEN 1 ELSE 0 END) / COUNT(*), 2) AS hospitalization_percent,
+    ROUND(100.0 * SUM(CASE WHEN ev.disposition = 'HOSPITALIZED' THEN 1 ELSE 0 END) / COUNT(*) OVER(PARTITION BY ev.emergency_level), 2) AS hospitalization_percent,
     CASE
         WHEN ev.referred_department_id IS NULL THEN 'No referred department'
         ELSE d.department_name
